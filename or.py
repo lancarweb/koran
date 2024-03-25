8dc9d5f6066f27d3be20ae585ecddf663e463f61
M KHOIRUDDIN <103399266+lancarweb@users.noreply.github.com>
2024-03-25 22:18:22 +0700

    Update or.py

--- or.py
+++ or.py
@@ -11,7 +11,7 @@ spark = SparkSession.builder \
 
 jdbcDF = spark.read.format("jdbc") \
     .option("url", "jdbc:oracle:thin:@192.168.77.12:1521/FREE") \
-    .option("query", "from * from POWER_PLANTS") \
+    .option("query", "select * from POWER_PLANTS") \
     .option("user", "system") \
     .option("password", "password123") \
     .option("driver", "oracle.jdbc.driver.OracleDriver") \
