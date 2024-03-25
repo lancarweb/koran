2eace5e7e5b7dc3f6b8937ec85e583a946684dcb
M KHOIRUDDIN <103399266+lancarweb@users.noreply.github.com>
2024-03-25 22:15:59 +0700

    Update or.py

--- or.py
+++ or.py
@@ -10,7 +10,7 @@ spark = SparkSession.builder \
     .getOrCreate()
 
 jdbcDF = spark.read.format("jdbc") \
-    .option("url", "jdbc:system:password123:@192.168.77.12:1521:FREE") \
+    .option("url", "jdbc:oracle:thin:@192.168.77.12:1521/FREE") \
     .option("query", "from * from POWER_PLANTS") \
     .option("user", "system") \
     .option("password", "password123") \
