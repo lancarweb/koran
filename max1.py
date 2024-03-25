6dc3b90a5bf01b680c25e9b3a5ef4225b8ce08f4
M KHOIRUDDIN <103399266+lancarweb@users.noreply.github.com>
2024-03-25 22:33:59 +0700

    Add files via upload

---
+++ max1.py
@@ -0,0 +1,19 @@
+# oracle-example.py
+from pyspark.sql import SparkSession
+
+appName = "PySpark Example - Oracle Example"
+master = "local"
+# Create Spark session
+spark = SparkSession.builder \
+    .appName(appName) \
+    .master(master) \
+    .getOrCreate()
+
+jdbcDF = spark.read.format("jdbc") \
+    .option("url", "jdbc:oracle:thin:@192.168.200.14:1521/MAXDB") \
+    .option("query", "select * from MATRECTRANS") \
+    .option("user", "maximo") \
+    .option("password", "maximo") \
+    .option("driver", "oracle.jdbc.driver.OracleDriver") \
+    .load()
+jdbcDF.toJSON().first()
